c2485991897c4c1aa67edaa6cd296bb6524bef83
Matt Oskamp <matt.oskamp@stantive.com>
2015-04-18 17:34:32 -0400

    unity steer

--- Ludum Dare 32/Assets/Scripts/CharaterMover.cs
+++ Ludum Dare 32/Assets/Scripts/CharaterMover.cs
@@ -19,5 +19,9 @@ public class CharaterMover : MonoBehaviour {
 	void FixedUpdate() {
 		transform.position += transform.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
 		transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+		Vector3 objectPos = Camera.main.WorldToScreenPoint(this.transform.position);
+		Vector3 dir = Input.mousePosition - objectPos; 
+		//transform.rotation = Quaternion.Euler (new Vector3 (0, 0, Mathf.Atan2 (dir.y, dir.x) * Mathf.Rad2Deg));
 	}
 }

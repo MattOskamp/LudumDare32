bff874a71fd95f2795597b7e27bff8ecc666808b
Matt Oskamp <matt.oskamp@stantive.com>
2015-04-18 12:10:24 -0400

    Shadow stuff

---
+++ Ludum Dare 32/Assets/Scripts/CharaterMover.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharaterMover : MonoBehaviour {
+
+	CharacterController controller;
+
+	// Use this for initialization
+	void Start () {
+		controller = new CharacterController ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

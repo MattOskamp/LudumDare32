f6f205b178972a363de13b725d8950dcc855c45a
Matt Oskamp <matt.oskamp@stantive.com>
2015-04-18 12:24:35 -0400

    updating

--- Ludum Dare 32/Assets/Scripts/CharaterMover.cs
+++ Ludum Dare 32/Assets/Scripts/CharaterMover.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CharaterMover : MonoBehaviour {
 
 	CharacterController controller;
+	public float speed = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -13,5 +14,10 @@ public class CharaterMover : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+	}
+
+	void FixedUpdate() {
+		transform.position += transform.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+		transform.position += transform.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
 	}
 }

9786e39179ddcb8d97183ef08e39f8fc55abeb10
Adrian Schneider <warclam@Artemis.local>
2015-04-18 14:49:25 -0400

    Removed dud "Test" script, added PlayerController

---
+++ Ludum Dare 32/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField] private float speed;
+	private CharacterController cc;
+
+	void Start ()
+	{
+		cc = GetComponent<CharacterController>();
+	}
+
+	void Update ()
+	{
+		if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0)
+		{
+			float angle = Mathf.Atan2(Mathf.Abs(Input.GetAxis("Vertical")), Mathf.Abs(Input.GetAxis("Horizontal")));
+
+			float xSpeed = speed*Mathf.Cos(angle);
+			if (Input.GetAxis("Horizontal") < 0)
+				xSpeed *= -1;
+			cc.Move(Vector3.right*Time.deltaTime*xSpeed);
+
+			float ySpeed = speed*Mathf.Sin(angle);
+			if (Input.GetAxis("Vertical") < 0)
+		        ySpeed *= -1;
+			cc.Move(Vector3.up*Time.deltaTime*ySpeed);
+		}
+		else if (Input.GetAxis("Horizontal") != 0)
+		{
+			cc.Move(Vector3.right*Time.deltaTime*speed*Input.GetAxis("Horizontal"));
+		}
+		else if (Input.GetAxis("Vertical") != 0)
+		{
+			cc.Move(Vector3.up*Time.deltaTime*speed*Input.GetAxis("Vertical"));
+		}
+	}
+
+}

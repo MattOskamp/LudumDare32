e28e67b9778326ad04b2d60f4c0c8831e1d9a450
Matt Oskamp <matt.oskamp@stantive.com>
2015-04-19 10:41:20 -0400

    ceiling fade

---
+++ Ludum Dare 32/Assets/Scripts/Ceiling.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Renderer))]
+public class Ceiling : MonoBehaviour {
+
+	public float fadeSpeed = 1.0f;
+	private Renderer renderer;
+	public bool turnDown = false;
+	// Use this for initialization
+	void Start () {
+		renderer = gameObject.GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (turnDown)
+			turnOpacityDown ();
+		else
+			turnOpacityUp ();
+	}
+
+	void OnTriggerEnter(Collider other) {
+		turnDown = true;
+	}
+
+	void OnTriggerExit(Collider other) {
+		turnDown = false;
+	}
+
+	private void turnOpacityDown() {
+		float alpha = renderer.material.color.a;
+		if (alpha > 0)
+			alpha -= Time.deltaTime * fadeSpeed;
+		Color col = renderer.material.color;
+		col.a = alpha;
+		renderer.material.color = col;
+	}
+
+	private void turnOpacityUp() {
+		float alpha = renderer.material.color.a;
+		if (alpha < 1)
+			alpha += Time.deltaTime * fadeSpeed;
+		Color col = renderer.material.color;
+		col.a = alpha;
+		renderer.material.color = col;
+	}
+}

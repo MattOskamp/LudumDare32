ee90b32e41fb33bea24b127c2f74dd3187c1be18
Adrian Schneider <warclam@Porphyra.local>
2015-04-19 11:07:11 -0400

    Strafemaster update

--- Ludum Dare 32/Assets/Scripts/PlayerController.cs
+++ Ludum Dare 32/Assets/Scripts/PlayerController.cs
@@ -17,19 +17,30 @@ public class PlayerController : MonoBehaviour {
 
 	void Update ()
 	{
+		
+		//facing direction
+		mouseDirection = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x)*Mathf.Rad2Deg-90;
+		transform.rotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
+		
+		/*if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+		{
+			transform.rotation = Quaternion.LookRotation(Vector3.right*Input.GetAxis("Horizontal") + Vector3.up*Input.GetAxis("Vertical"), Vector3.back);
+		}*/
+
 		//movement direction
 		if (Input.GetAxis("Horizontal") != 0 && Input.GetAxis("Vertical") != 0)
 		{
-			cc.Move(Vector3.right*Time.deltaTime*speed*Input.GetAxis("Horizontal")*Mathf.Sqrt(0.5f));
-			cc.Move(Vector3.up*Time.deltaTime*speed*Input.GetAxis("Vertical")*Mathf.Sqrt(0.5f));
+			cc.Move(transform.right*Time.deltaTime*speed*Input.GetAxis("Horizontal")*Mathf.Sqrt(0.5f));
+			cc.Move(transform.up*Time.deltaTime*speed*Input.GetAxis("Vertical")*Mathf.Sqrt(0.5f));
 		}
 		else if (Input.GetAxis("Horizontal") != 0)
 		{
-			cc.Move(Vector3.right*Time.deltaTime*speed*Input.GetAxis("Horizontal"));
+			cc.Move(transform.right*Time.deltaTime*speed*Input.GetAxis("Horizontal"));
 		}
 		else if (Input.GetAxis("Vertical") != 0)
 		{
-			cc.Move(Vector3.up*Time.deltaTime*speed*Input.GetAxis("Vertical"));
+			cc.Move(transform.up*Time.deltaTime*speed*Input.GetAxis("Vertical"));
 		}
 
 		if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
@@ -40,16 +51,6 @@ public class PlayerController : MonoBehaviour {
 		{
 			tor.SetBool("Walking", false);
 		}
-
-		//facing direction
-		mouseDirection = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-		mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x)*Mathf.Rad2Deg-90;
-		transform.rotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
-
-		/*if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
-		{
-			transform.rotation = Quaternion.LookRotation(Vector3.right*Input.GetAxis("Horizontal") + Vector3.up*Input.GetAxis("Vertical"), Vector3.back);
-		}*/
 	}
 
 }

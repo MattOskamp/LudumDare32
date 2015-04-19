f5809088b6d5866917856029aaa9f6b21d47d191
Matt Oskamp <matt.oskamp@stantive.com>
2015-04-19 12:08:10 -0400

    Flashlight functionality

---
+++ Ludum Dare 32/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+	public Transform target;
+	public bool on;
+	public Component vlsRadial;
+
+	// Use this for initialization
+	void Start () {
+		on = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		this.transform.position = target.transform.position;
+		//facing direction
+		Vector2 mouseDirection = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+		float mouseAngle = Mathf.Atan2(mouseDirection.y, mouseDirection.x)*Mathf.Rad2Deg + 90;
+		transform.rotation = Quaternion.AngleAxis(mouseAngle, Vector3.forward);
+	}
+}

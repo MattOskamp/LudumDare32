3fc36b571c493486e3e8153e67a14c48d4473585
Adrian Schneider <warclam@Porphyra.local>
2015-04-19 17:38:53 -0400

    newest monster brain

--- Ludum Dare 32/Assets/Scripts/MonsterBrain.cs
+++ Ludum Dare 32/Assets/Scripts/MonsterBrain.cs
@@ -9,39 +9,50 @@ public class MonsterBrain : MonoBehaviour {
 	private float angle;
 	private RaycastHit hit;
 
+	//state 0 is idle/frozen
+	//state 1 is chasing
+	//state 2 is dying
+	private int state = 0;
+
 	void Update ()
 	{
-		dir = (target.position - transform.position);
-		/*if(Physics.Raycast(transform.position, transform.up, out hit, speed))
-		{
-			if(hit.transform != transform && hit.transform != target)
-			{
-				dir += hit.normal*50;
-			}
-		}*/
 
-		leftR = transform.position + (transform.right * -0.5f);
-		rightR = transform.position + (transform.right * 0.5f);
-		
-		if(Physics.Raycast(leftR, transform.up, out hit, speed))
-		{
-			if(hit.transform != transform && hit.transform != target)
-			{
-				dir += hit.normal*20;
-			}
-		}
-		
-		if(Physics.Raycast(rightR, transform.up, out hit, speed))
-		{
-			if(hit.transform != transform && hit.transform != target)
-			{
-				dir += hit.normal*20;
-			}
-		}
 
-		angle = Mathf.Atan2(dir.y, dir.x)*Mathf.Rad2Deg-90;
-		//transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-		transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.AngleAxis(angle, Vector3.forward),Time.deltaTime);
-		transform.position += transform.up * speed * Time.deltaTime;
+		//if we're in chase mode, munt the player
+		if (state == 1)
+		{
+			dir = (target.position - transform.position);
+			/*if(Physics.Raycast(transform.position, transform.up, out hit, speed))
+			{
+				if(hit.transform != transform && hit.transform != target)
+				{
+					dir += hit.normal*50;
+				}
+			}*/
+
+			leftR = transform.position + (transform.right * -0.5f);
+			rightR = transform.position + (transform.right * 0.5f);
+			
+			if(Physics.Raycast(leftR, transform.up, out hit, speed))
+			{
+				if(hit.transform != transform && hit.transform != target)
+				{
+					dir += hit.normal*20;
+				}
+			}
+			
+			if(Physics.Raycast(rightR, transform.up, out hit, speed))
+			{
+				if(hit.transform != transform && hit.transform != target)
+				{
+					dir += hit.normal*20;
+				}
+			}
+
+			angle = Mathf.Atan2(dir.y, dir.x)*Mathf.Rad2Deg-90;
+			//transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+			transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.AngleAxis(angle, Vector3.forward),Time.deltaTime);
+			transform.position += transform.up * speed * Time.deltaTime;
+		}
 	}
 }

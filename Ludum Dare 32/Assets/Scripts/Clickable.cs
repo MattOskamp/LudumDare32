36d4dd5ebf2ca43beea774293fdb795c1caf5796
Cheryl Savery <cheryl.savery@gmail.com>
2015-04-19 16:14:16 -0400

    Added Title Scene

---
+++ Ludum Dare 32/Assets/Scripts/Clickable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void MouseEventHandler (Vector3 position);
+
+public enum MouseEventType
+{
+	Down,
+	Up,
+	RightDown,
+	RightUp
+}
+
+public class Clickable : MonoBehaviour
+{
+	public event MouseEventHandler DownAction = delegate {};
+	public event MouseEventHandler UpAction = delegate {};
+	public event MouseEventHandler RightDownAction = delegate {};
+	public event MouseEventHandler RightUpAction = delegate {};
+
+	public void FireEvent (MouseEventType t, Vector3 position)
+	{
+		if (t == MouseEventType.Down)
+		{
+			DownAction(position);
+		}
+		else if (t == MouseEventType.Up)
+		{
+			UpAction(position);
+		}
+		else if (t == MouseEventType.RightDown)
+		{
+			RightDownAction(position);
+		}
+		else if (t == MouseEventType.RightUp)
+		{
+			RightUpAction(position);
+		}
+	}
+}
---
+++ Ludum Dare 32/Assets/Scripts/MenuButtonScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonScript : MonoBehaviour {
+
+	Color normalColor = new Color(0.83f, 0.83f, 0.647f);
+	Color highlightColor = new Color(0.8f,0.8f,0.2f);
+
+	void Awake ()
+	{
+		GetComponent<Clickable>().DownAction += OnDownAction;
+		GetComponent<Clickable>().UpAction += OnUpAction;
+	}
+	
+
+	
+	void OnDownAction (Vector3 position)
+	{		
+		this.GetComponent<SpriteRenderer>().color = highlightColor;
+	}
+	
+	void OnUpAction (Vector3 position)
+	{
+		this.GetComponent<SpriteRenderer>().color = normalColor;
+
+		if ( this.name == "PlayButton")
+		{
+			this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+			this.transform.FindChild("Loading").gameObject.SetActive(true);
+			Application.LoadLevel("Shadows");
+
+		}
+	}
+	
+	void OnMouseEnter()
+	{
+		if (!Input.touchSupported || Input.touches.Length > 0)
+		{
+			this.GetComponent<SpriteRenderer>().color = highlightColor;
+		}
+	}
+	
+	void OnMouseExit()
+	{
+		this.GetComponent<SpriteRenderer>().color = normalColor;
+	}
+	
+}

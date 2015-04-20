38081946eff4657b6821899edb424359e0a5d471
Cheryl Savery <cheryl.savery@gmail.com>
2015-04-20 09:15:56 -0400

    Key GUI added

---
+++ Ludum Dare 32/Assets/Scripts/KeyGuiScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class KeyGuiScript : MonoBehaviour {
+
+	private PlayerController playerController;
+
+
+	// Use this for initialization
+	void Start () {
+		playerController = GameObject.Find("MainCharacter").GetComponent<PlayerController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Text>().text = "" + playerController.keyCount + "/6";
+	
+	}
+}
